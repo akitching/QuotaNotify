5e9f84591a2084182b16165a18bb8f0f09098008
Alex Kitching <alex@kitching.info>
2014-10-21 14:29:03 +0100

    Create Config and Drive classes
    * Move configuration details into Config class
    * Create Drive class to represent monitored drives
    * Implement reading configuration settings from Windows registry

--- NotificationIcon.cs
+++ NotificationIcon.cs
@@ -103,21 +103,111 @@ namespace QuotaNotify
 		#endregion
 	}
 	
-	public class DiskChecker : Form
+	class Drive : IEquatable<Drive>
 	{
-		static System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+		private char letterValue;
+		private double percentFreeValue;
 		
-		private Dictionary<char,double> percentFreeD;
-		private bool warning;
-		private string message;
-		private List<char> drives;
-		
-		private DateTime nextCheck;
-		
-		public DiskChecker()
+		public Drive(char letter)
 		{
-			this.Hide();
-			drives = new List<char>();
+			letterValue = letter;
+			percentFreeValue = 100.0f;
+		}
+		
+		#region Equals and GetHashCode implementation
+		public override bool Equals(object obj)
+		{
+			Drive other = obj as Drive;
+			if (other == null)
+				return false;
+			return this.letterValue == other.letterValue;
+		}
+
+		public bool Equals(Drive other)
+		{
+			if (other == null)
+				return false;
+			return (this.letterValue.Equals(other.letterValue));
+		}
+		
+		public override int GetHashCode()
+		{
+			int hashCode = 0;
+			unchecked {
+				hashCode += 1000000007 * letterValue.GetHashCode();
+			}
+			return hashCode;
+		}
+
+		public static bool operator ==(Drive lhs, Drive rhs)
+		{
+			if (ReferenceEquals(lhs, rhs))
+				return true;
+			if (ReferenceEquals(lhs, null) || ReferenceEquals(rhs, null))
+				return false;
+			return lhs.Equals(rhs);
+		}
+
+		public static bool operator !=(Drive lhs, Drive rhs)
+		{
+			return !(lhs == rhs);
+		}
+		#endregion
+
+		public char letter()
+		{
+			return letterValue;
+		}
+
+		public double percentFree()
+		{
+			return percentFreeValue;
+		}
+
+		public void percentFree(double percentFreeValue)
+		{
+			this.percentFreeValue = percentFreeValue;
+		}
+	}
+
+	class Config
+	{
+		private List<Drive> driveList;
+		private int initialIntervalValue;
+		private int checkIntervalValue;
+		private int warnPercentValue;
+		private int warnBelowValue;
+		private string warnMessageValue;
+		private bool obsessValue;
+
+		public Config()
+		{
+			driveList = new List<Drive>();
+			// Set default values
+			initialIntervalValue = 5000;
+			checkIntervalValue = 300000;
+			warnPercentValue = 10;
+			warnBelowValue = 100 * 1024 * 1024;
+			warnMessageValue = null;
+			obsessValue = false;
+			// Override defaults with cutomized settings
+			this.loadFromFile();
+			this.loadFromRegistry();
+			if (driveList.Count == 0)
+			{
+				// No drives configured
+				// Default to home drive H
+				driveList.Add(new Drive('H'));
+			}
+		}
+
+		private void loadFromFile()
+		{
+			// TODO: Load config data from file in install directory
+		}
+
+		private void loadFromRegistry()
+		{
 			string[] driveList = null;
 			string keyLocation = "SOFTWARE\\Amalgam";
 			RegistryKey registry64 = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
@@ -132,66 +222,176 @@ namespace QuotaNotify
 
 				if (key != null)
 				{
-					// Read drive list from registry
-					driveList = (string[]) key.GetValue("Drives");
-				}
-				if (driveList == null)
-				{
-					// No drives in registry
-					// Default to home drive H
-					drives.Add('H');
-				}
-				else
-				{
+					initialIntervalValue = readRegistryKey(initialIntervalValue, "initialInterval", key);
+					checkIntervalValue = readRegistryKey(checkIntervalValue, "checkInterval", key);
+					warnPercentValue = readRegistryKey(warnPercentValue, "warnPercent", key);
+					warnBelowValue = readRegistryKey(warnBelowValue, "warnBelow", key);
+					warnMessageValue = readRegistryKey(warnMessageValue, "warnMessage", key);
+					obsessValue = readRegistryKey(obsessValue, "obsess", key);
+
+					driveList = readRegistryKey(driveList, "Drives", key);
 					foreach (string drive in driveList)
 					{
-						drives.Add(drive.ToCharArray()[0]);
+						this.driveList.Add(new Drive(drive.ToCharArray()[0]));
+					}
+					foreach (Drive drive in this.driveList) {
+						drive.percentFree(100.0f);
 					}
 				}
-
-				percentFreeD = new Dictionary<char,double>();
-				foreach (char drive in drives) {
-					percentFreeD[drive] = 100.0f;
-				}
-
-				nextCheck = DateTime.Now;
-				timer.Tick += new EventHandler(checkDriveSpace);
-				timer.Interval = 5000;
-				timer.Start();
-				this.Hide();
 			} catch (Exception ex) {
 				MessageBox.Show( "Error " + ex.ToString() );
+			} finally {
+				registry32.Close();
+				registry64.Close();
+				key.Close();
 			}
+		}
+
+		private bool readRegistryKey(bool variable, string name, RegistryKey key)
+		{
+			object val = key.GetValue(name);
+			if (val == null)
+			{
+				return variable;
+			}
+			else
+			{
+				if ((int) val > 0)
+					return true;
+				else
+					return false;
+			}
+		}
+
+		private int readRegistryKey(int variable, string name, RegistryKey key)
+		{
+			object val = key.GetValue(name);
+			if (val == null)
+			{
+				return variable;
+			}
+			else
+			{
+				return (int) val;
+			}
+		}
+
+		private string readRegistryKey(string variable, string name, RegistryKey key)
+		{
+			object val = key.GetValue(name);
+			if (val == null)
+			{
+				return variable;
+			}
+			else
+			{
+				return (string) val;
+			}
+		}
+
+		private string[] readRegistryKey(string[] variable, string name, RegistryKey key)
+		{
+			object val = key.GetValue(name);
+			if (val == null)
+			{
+				return variable;
+			}
+			else
+			{
+				return (string[]) val;
+			}
+		}
+
+		public List<Drive> drives()
+		{
+			return driveList;
+		}
+
+		public int initialInterval()
+		{
+			return initialIntervalValue;
+		}
+
+		public int checkInterval()
+		{
+			return checkIntervalValue;
+		}
+
+		public int warnPercent()
+		{
+			return warnPercentValue;
+		}
+
+		public int warnBelow()
+		{
+			return warnBelowValue;
+		}
+
+		public string warnMessage()
+		{
+			return warnMessageValue;
+		}
+
+		public bool obsess()
+		{
+			return obsessValue;
+		}
+	}
+
+	public class DiskChecker : Form
+	{
+		static System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+		private bool warning;
+		private string message;
+		private Config config;
+
+		private DateTime nextCheck;
+
+		public DiskChecker()
+		{
+			this.Hide();
+			config = new Config();
+
+			nextCheck = DateTime.Now;
+			timer.Tick += new EventHandler(checkDriveSpace);
+			timer.Interval = config.initialInterval();
+			timer.Start();
+			this.Hide();
 		}
 
 		private void checkDriveSpace(Object sender, EventArgs args)
 		{
-			if (timer.Interval < 300000)
+			if (timer.Interval < config.checkInterval())
 			{
-				timer.Interval = 300000;
+				timer.Interval = config.checkInterval();
 			}
 			warning = false;
 			message = "";
+			// TODO: Refactor foreach to loop over config.drives rather than DriveInfo.GetDrives
 			foreach (DriveInfo drive in DriveInfo.GetDrives())
 			{
 				if (drive.IsReady)
 				{
 					double percentFree = ((double)drive.TotalFreeSpace / drive.TotalSize) * 100;
 					char driveLetter = drive.Name.ToCharArray()[0];
-					if ( drives.Contains(driveLetter) )
+					if ( config.drives().Exists(x => x.letter() == driveLetter) )
 					{
-						if ((percentFree < 10) && (percentFree < percentFreeD[driveLetter]) && (drive.TotalFreeSpace < (100 * 1024 * 1024)))
+						Drive drv = config.drives().Find(x => x.letter() == driveLetter);
+						if ((percentFree < config.warnPercent()) && ((percentFree < drv.percentFree() || config.obsess() == true)) && (drive.TotalFreeSpace < config.warnBelow()))
 						{
 							warning = true;
 							message += "Drive " + drive.Name + " has only " + String.Format("{0:F2}", percentFree) + "% free space.\n";
 						}
-						percentFreeD[driveLetter] = percentFree;
+						drv.percentFree(percentFree);
 					}
 				}
 			}
 			if (warning)
 			{
 				message += "Please delete unnecessary files.\n";
+				if (config.warnMessage() != null)
+					message += config.warnMessage();
 				MessageBox.Show (message, "Low disk space");
 				warning = false;
 				message = "";
